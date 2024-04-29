95d3fc59fcab9b802b6b65c1954720281fa43670
Olga Kotin <o.b.baglay@gmail.com>
2024-04-29 20:32:21 +0300

    HW Tests

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,4 +1,18 @@
 package tests;
 
-public class BaseTest {
+import config.ConfigManager;
+import org.testng.asserts.SoftAssert;
+import pages.*;
+
+public class BaseTest extends ConfigManager {
+    SoftAssert softAssert = new SoftAssert();
+    MainPage mainPage = new MainPage();
+    ElementsPage elementsPage = new ElementsPage();
+    AlertsWindowsPage alertsWindowsPage = new AlertsWindowsPage();
+    AlertsPage alertsPage = new AlertsPage();
+    SliderPage sliderPage = new SliderPage();
+    HandlersPage handlersPage = new HandlersPage();
+    StudentRegistrationPage studentRegistrationPage = new StudentRegistrationPage();
+
+
 }
--- src/test/java/tests/hardcoding/FirstTest.java
+++ src/test/java/tests/hardcoding/FirstTest.java
@@ -1,9 +1,12 @@
 package tests.hardcoding;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -14,8 +17,13 @@ public class FirstTest {
 
     @Test
     public void testElementsPage() {
-        // TODO with webdrivermanager
-        WebDriver driver = new FirefoxDriver();
+        //WebDriver driver = new FirefoxDriver();
+        //----------------- with WebDriverManager
+        WebDriver driver;
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        driver = new ChromeDriver(options);
+        //-------------
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
         driver.navigate().to("https://demoqa.com/");
